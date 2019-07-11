e50df68edeea45189dcd84ebb8c61bebcc7f27fe
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-07-11 23:51:42 +0530

    new schema migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_100525) do
+ActiveRecord::Schema.define(version: 2019_07_11_172303) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2019_07_10_100525) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.bigint "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
