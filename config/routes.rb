8bf0899fa3fbe5fe8ff5d0832218db03ea8e7200
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-07-11 22:18:49 +0530

    some documentation added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :contacts
-  root 'contacts#index'
+  root 'contacts#index' # change root to contact index page
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
