1ae46fb49a3e122cacf1ae930f9f14b08ee6fb0d
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-07-13 13:46:30 +0530

    fix some errrs

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
     # add validation
-    validates :name, :email, :phone2, :phone, presence: true
+    validates :name, :email,  :phone, presence: true
     has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
