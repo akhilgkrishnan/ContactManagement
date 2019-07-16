bfdc96720e9dcc9010cb30f57f4675b65e71678c
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-07-16 15:00:40 +0530

    sample change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_121757) do
+ActiveRecord::Schema.define(version: 2019_07_16_073546) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.text "address"
+    t.integer "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_addresses_on_contact_id"
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
