e242419c344598494e38cf8ffaf75ba93344d064
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-07-16 14:59:40 +0530

    add nested from remove and add

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,46 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+$(document).on('turbolinks:load', function() {
 
+    $('form').on('click', '.remove_record', function(event) {
+      $(this).prev('input[type=hidden]').val('1');
+      $(this).closest('div').hide();
+      
+      return event.preventDefault();    
+    });
+  
+    $('form').on('click', '.add_fields', function(event) {
+      var regexp, time;
+      time = new Date().getTime();
+      regexp = new RegExp($(this).data('id'), 'g');
+      $('.fields').append($(this).data('fields').replace(regexp, time));
+      return event.preventDefault();
+    });
+
+    $('form').on('click', '.remove_record2', function(event) {
+        $(this).prev('input[type=hidden]').val('1');
+        $(this).closest('div').hide();
+        
+        return event.preventDefault();    
+      });
+
+      $('form').on('click', '.add_fields1', function(event) {
+        var regexp, time;
+        time = new Date().getTime();
+        regexp = new RegExp($(this).data('id'), 'g');
+        $('.fields2').append($(this).data('fields').replace(regexp, time));
+        return event.preventDefault();
+      });  
+  });
+  
+
+
+  
+/*function add_fields(link, association, content) {
+    var new_id = new Date().getTime();
+    var regexp = new RegExp("new_" + association, "g")
+    $(link).up().insert({
+      before: content.replace(regexp, new_id)
+    });
+  }*/
