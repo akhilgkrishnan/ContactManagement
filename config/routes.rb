ebb323665926d2aaf041dadb532e41dfd19a8c95
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-07-17 23:46:55 +0530

    devise method added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  #get 'home/index'
+  devise_for :users, controllers: { registrations: "registrations "}
   #resources :phones
   resources :contacts
-  root 'contacts#index' # change root to contact index page
+  root 'home#index' # change root to contact index page
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
