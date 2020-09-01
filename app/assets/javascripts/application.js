3d1f02240511b09a94759113328a9bbac879c906
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2020-09-01 22:01:08 +0530

    removed unwanted lines

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,12 +51,3 @@ $(document).on('turbolinks:load', function() {
   });
   
 
-
-  
-/*function add_fields(link, association, content) {
-    var new_id = new Date().getTime();
-    var regexp = new RegExp("new_" + association, "g")
-    $(link).up().insert({
-      before: content.replace(regexp, new_id)
-    });
-  }*/
